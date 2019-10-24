05ede57639757d00337eba01f9213dc00cd0730b
Christopher-Nall <bigcmn@gmail.com>
2019-10-24 15:24:16 -0500

    MISTAKES WERE MADE

--- Mistakes.py
+++ Mistakes.py
@@ -2,3 +2,19 @@
 
 # Concatenation
 # 2 strings or more and put the together
+
+firstName = "Yuri"
+lastName = "tarded"
+
+print(firstName + " " + lastName)
+
+name = firstName + " " + lastName
+lastFirst = lastName + " " + firstName
+
+print(name)
+print(lastFirst)
+
+# repitition
+
+print("whyyy"*20)
+print("o"*20)
