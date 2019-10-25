efb6ad84d3edc61493858de9e97d1c7d8a29cb1f
Christopher-Nall <bigcmn@gmail.com>
2019-10-25 15:29:55 -0500

    MISTAKES WERE MADE

--- Mistakes.py
+++ Mistakes.py
@@ -40,3 +40,48 @@ print(name[-1])
 for i in range(1, len(name)):
     print(name[i])
 
+# Slicing and Dicing
+
+print(name[0:3])
+print(name[0:10])
+
+print(name[-3])
+
+for i in range(0, len(name)):
+    print(name[i])
+
+# Slicing and dicing
+
+print(name[-4:8])
+
+for i in range(0, len(name)+1):
+    print(name[0:i])
+
+if "y" not in name:
+    print("y is not in name")
+
+
+
+# Method     Use example      Explanation
+# Center
+# ljust
+# rjust
+# upper
+# lower
+# index
+# rindex
+# find
+# rfind
+# replace
+
+Mine = "not yours"
+notYours = " mine"
+
+print(Mine + notYours)
+string = (Mine + notYours)
+
+print(string)
+string.center(5, Mine)
+print(string.center(5, Mine))
+
+print(str.ljust(Mine, 5[ notYours]))
