a5b280c647838e72ef0b6924c9cc3d7248a88b3b
Christopher-Nall <bigcmn@gmail.com>
2019-10-28 15:54:43 -0500

    MISTAKES WERE MADE

--- mistakes2.py
+++ mistakes2.py
@@ -1,6 +1,6 @@
 def letterToIndex(letter):
     from string import ascii_lowercase
-    alphabet = ascili_lowercase + ' '
+    alphabet = ascii_lowercase + ' '
     idx = alphabet.find(letter)
     if idx == -1:       # Means it wasnt in the alphabet
         print("error", letter, "is not in the alphabet")
@@ -12,8 +12,14 @@ def indexToLetter(idx):
     letter = ''
     if idx >= len(alphabet):
         print("error", idx, "is to large")
-    elif idx < 0
+    elif idx < 0:
         print("error", idx, "is to small")
     else:
         letter = alphabet[idx]
     return letter
+
+
+from mistakes2 import *
+print(letterToIndex('a'))
+
+print(indexToLetter(44))
