853f7f86a2c10de8ac1b89cf115e0755319c5c5b
Christopher-Nall <bigcmn@gmail.com>
2019-10-29 15:28:20 -0500

    Encryption is hard

---
+++ Crypto.py
@@ -0,0 +1,16 @@
+# Transposition Cypher
+
+# encryption first
+
+def scrambleToEncrypt(plaintext):
+    evenChar = ""
+    oddChar = ""
+    charCount = 0
+    for ch in plaintext:
+        if charCount % 2 == 0
+            evenChar = evenChar + ch
+        else:
+            oddChar = evenChar + ch
+        charCount = charCount + 1
+    cipherText = oddChar + evenChar
+    return cipherText
--- mistakes2.py
+++ mistakes2.py
@@ -1,4 +1,4 @@
-def letterToIndex(letter):
+defletterToIndex(letter):
     from string import ascii_lowercase
     alphabet = ascii_lowercase + ' '
     idx = alphabet.find(letter)
@@ -23,3 +23,9 @@ from mistakes2 import *
 print(letterToIndex('a'))
 
 print(indexToLetter(44))
+
+this_is_a_secret_meassage_that_i_want_to_transmit
+
+
+ti_s_a_sce_masg_ta__at_rnmt
+hs_i__ert_esse_ht_i_wn_t_tasi
